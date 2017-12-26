b1d8ff311b65f451ed2dbaa50ffe265b9010cea0
Jpsr2 <jpsarodrigues@gmail.com>
2017-12-26 19:08:38 +0000

    Added harmonic oscillator potential to test in Gross-Pitaevskii Equation

--- tester_pitaevskii.py
+++ tester_pitaevskii.py
@@ -18,8 +18,11 @@ def V(x,y,V0,R0):
 	V_[index] = 0
 	return V_
 
+def V_harmonic(x,y):
+	return x**2/2
+
 def psi_0(X,Y):
-	return np.exp(-(((X-0.2)**2 + (Y-0.1)**2)/0.1**2)**4)*np.exp(1.j*100.*X)
+	return np.exp(-(((X-0.)**2 + (Y-0.)**2)/0.1**2))*np.exp(1.j*0.*X)
 
 #Initializing system and equation
 system = SYSTEM()
@@ -35,6 +38,7 @@ dh = 2.*L/(N-1)
 
 X, Y = np.mgrid[-L:L+dh:dh,-L:L+dh:dh]
 
+
 k_line = np.fft.fftfreq(N,dh)
 k_x, k_y = np.meshgrid(k_line,k_line)
 K2 = k_x**2 + k_y**2
@@ -49,7 +53,8 @@ N_steps = 100
 term1 = TERM(0.5*K2,'Momentum','P_squared')
 eq1.add_term(term1)
 
-term2 = TERM(V(X,Y,V0,R0),'Position','Binding Potential')
+#term2 = TERM(V(X,Y,V0,R0),'Position','Binding Potential')
+term2 = TERM(V_harmonic(X,Y),'Position','Potential')
 eq1.add_term(term2)
 
 f = lambda eq: np.abs(eq.solution)**2
